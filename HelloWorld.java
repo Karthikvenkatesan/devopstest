6352e2d93a2e460f0a52528ad1e316bf5fb819a4
Karthikvenkatesan <karthik.venkatesan1@tcs.com>
2017-05-01 00:06:52 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,16 @@
 public class HelloWorld 
 {
-   public static void main(String[] args) 
-   {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World Mr Karthik! . . Aha. Changed again 6");
-   }
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		HelloWorld helloWorld = new HelloWorld();
+		String strRet = helloWorld.sayHello("Karthik");
+		System.out.println("Str Ret = "+ strRet);
+	}
+	
+	public String sayHello(String strTo) 
+	{
+		String strRet = "Hello Mr. " + strTo; 
+		return strRet;		
+	}
 }
